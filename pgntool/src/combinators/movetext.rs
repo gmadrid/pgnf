8ec82cf8a4c95f0fe639de75788f229ee09fd942
George Madrid <gmadrid@gmail.com>
2021-11-18 11:49:27 -0500

    qseq! works awkwardly

--- pgntool/src/combinators/movetext.rs
+++ pgntool/src/combinators/movetext.rs
@@ -87,33 +87,45 @@ mod test {
         )
     }
 
-//     #[test]
-//     fn test_short_sequence() {
-//         let matcher = element_sequence_matcher();
-//
-//         assert_eq!(matcher.parse("1. e4 c6 2. d4 d5 3. Nc3 dxe4").unwrap(),
-//                    ElementSequence {
-//                        members: vec![
-//
-//                        ]
-//                    }
-//         );
-//     }
-//
-//     #[test]
-//     fn test_short_sequence_with_newline() {
-//         let matcher = element_sequence_matcher();
-//
-//         // TODO: make a macro to make this feasible.
-//         assert_eq!(matcher.parse(r#"1. e4 c6 2. d4 d5 3. Nc3 dxe4 4. Nxe4 Nf6 5. Bd3 Nbd7 6. Nxf6+ Nxf6 7. Bf4 e6 8.
-// c3 Bd6 9. Bxd6 Qxd6 "#).unwrap(),
-//                    ElementSequence {
-//                        members: vec![
-//
-//                        ]
-//                    }
-//         );
-//     }
+    macro_rules! qseq {
+        ($($x:ident $v:expr),+) => { ElementSequence { members: vec![ $(member!($x $v),)+ ] } };
+    }
+
+    macro_rules! member {
+        ( san $sanmove:expr ) => {
+            SequenceMember::Elem(ElementP::SanMove($sanmove.to_string()))
+        };
+        ( mv $movenum:expr) => {
+            SequenceMember::Elem(ElementP::MoveNumber($movenum))
+        };
+    }
+
+    trait ToEl {
+        fn to_element(&self) -> ElementP;
+    }
+
+    
+
+    #[test]
+    fn test_short_sequence() {
+        let matcher = element_sequence_matcher();
+
+        assert_eq!(
+            matcher.parse("1. e4 c6 2. d4 d5 3. Nc3 dxe4").unwrap(),
+            qseq!(mv 1, san "e4", san "c6", mv 2, san "d4", san "d5", mv 3, san "Nc3", san "dxe4")
+        );
+    }
+
+        #[test]
+        fn test_short_sequence_with_newline() {
+            let matcher = element_sequence_matcher();
+
+            // TODO: make a macro to make this feasible.
+            assert_eq!(matcher.parse(r#"1. e4 c6 2. d4 d5 3. Nc3 dxe4 4. Nxe4 Nf6 5. Bd3 Nbd7 6. Nxf6+ Nxf6 7. Bf4 e6 8.
+    c3 Bd6 9. Bxd6 Qxd6 "#).unwrap(),
+                qseq!()
+            );
+        }
 
     #[test]
     fn test_element_sequence_matcher() {
